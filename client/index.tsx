21c9b4cdaccd8e9019190e355d3e708027678ae5
melvinchia3636 <melvinchia623600@gmail.com>
2026-01-02 16:14:00 +0800

    fix(client): remove OTP wall

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,8 @@
-import forgeAPI from '@/utils/forgeAPI'
-import { WithMasterPassword, WithOTP } from 'lifeforge-ui'
+import { WithMasterPassword } from 'lifeforge-ui'
 import { useAuth } from 'shared'
 import type { InferOutput } from 'shared'
+
+import forgeAPI from '@/utils/forgeAPI'
 
 import ContentContainer from './components/ContentContainer'
 
@@ -13,28 +14,16 @@ function Passwords() {
   const { userData } = useAuth()
 
   return (
-    <>
-      <WithOTP
-        controllers={{
-          getChallenge: forgeAPI.passwords.master.getChallenge,
-          verifyOTP: forgeAPI.passwords.master.validateOTP,
-          generateOTP: forgeAPI.user.auth.generateOTP
-        }}
-      >
-        <WithMasterPassword
-          controllers={{
-            createPassword: forgeAPI.passwords.master.create,
-            getChallenge: forgeAPI.passwords.master.getChallenge,
-            verifyPassword: forgeAPI.passwords.master.verify
-          }}
-          hasMasterPassword={!!userData?.hasMasterPassword}
-        >
-          {masterPassword => (
-            <ContentContainer masterPassword={masterPassword} />
-          )}
-        </WithMasterPassword>
-      </WithOTP>
-    </>
+    <WithMasterPassword
+      controllers={{
+        createPassword: forgeAPI.passwords.master.create,
+        getChallenge: forgeAPI.passwords.master.getChallenge,
+        verifyPassword: forgeAPI.passwords.master.verify
+      }}
+      hasMasterPassword={!!userData?.hasMasterPassword}
+    >
+      {masterPassword => <ContentContainer masterPassword={masterPassword} />}
+    </WithMasterPassword>
   )
 }
 
