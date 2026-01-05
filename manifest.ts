80d86f9781853809cae359e277e6fc4c3ecac2ad
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:02:41 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Passwords',
-  icon: 'tabler:key',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Confidential'
 } satisfies ModuleConfig
